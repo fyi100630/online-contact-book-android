6804f765aed3a170b83fc80d7ddf08a268cd1e36
fyi100630 <fyi100630@users.noreply.github.com>
2026-09-15 21:58:55 +0800

    chore: bump version to v1.7.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
         applicationId = "com.rhythmbyte.contactbook"
         minSdk = 26
         targetSdk = 35
-        versionCode = 172
-        versionName = "1.7.2"
+        versionCode = 173
+        versionName = "1.7.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
